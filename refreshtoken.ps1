6f08777047a694155de5aa40a44439c5bde57404
msheldont4e <m.sheldon@tools4ever.com>
2020-08-18 06:17:05 -0700

    Create refreshtoken.ps1

---
+++ refreshtoken.ps1
@@ -0,0 +1,19 @@
+$requestUri = "https://www.googleapis.com/oauth2/v4/token"
+$clientId = "<put your client id here>"
+$clientSecret = "<put your client secret here>"
+$redirectUri = "http://localhost/oauth2callback"
+ 
+$code = "<put your authorization code here>"
+ 
+$body = @{
+    code=$code;
+    client_id=$clientId;
+    client_secret=$clientSecret;
+    redirect_uri=$redirectUri;
+    grant_type="authorization_code"; # Fixed value
+};
+ 
+$tokens = Invoke-RestMethod -Uri $requestUri -Method POST -Body $body;
+ 
+# Store refreshToken
+Set-Content $PSScriptRoot"\refreshToken.txt" $tokens.refresh_token
