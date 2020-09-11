bc3638bafd61ce7952fac71a1afc2d62a8bbcc36
Mike Sheldon <m.sheldon@tools4ever.com>
2020-09-11 07:56:23 -0700

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -7,28 +7,26 @@ $refreshToken = "<put your refreshtoken here>"
 $success = $False;
 $p = $person | ConvertFrom-Json
 $aRef = $accountReference | ConvertFrom-Json;
-$auditMessage = "Account for person " + $p.DisplayName + " not updated successfully";
- 
-$defaultDomain = "yourdomain.com";
+$auditMessage = "Account for person $($p.DisplayName) not updated successfully";
  
 #Change mapping here
 #For all of the supported attributes please check https://developers.google.com/admin-sdk/directory/v1/guides/manage-users
 $account = [PSCustomObject]@{
-    primaryEmail = $p.Contact.Business.Email.split("@")[0] + "@" + $defaultDomain
     name = @{
-                givenName = $p.Name.NickName
-                familyName = $p.Name.FamilyName
-                fullName = ($p.Name.NickName + " " + $p.Name.FamilyName)
+                givenName = "$($p.Name.NickName)"
+                familyName = "$($p.Name.FamilyName)"
+                fullName = "$($p.Name.NickName) $($p.Name.FamilyName)"
             }
     externalIds =  @(@{
-                        value = $p.ExternalId
+                        value = "$($p.ExternalId)"
                         type = "organization";
                     })
     organizations = @(@{
-                        title = ($p.primaryContract.Title.name)
-                        #department = ($p.primaryContract.custom.TeamDesc)
-                        #costCenter = ($p.primaryContract.costCenter.ExternalID)
+                        title = "$($p.primaryContract.Title.name)"
+                        #department = "$($p.primaryContract.custom.TeamDesc)"
+                        #costCenter = "$($p.primaryContract.costCenter.ExternalID)"
                     })
+    orgUnitPath = "/Users"
 }
  
 try{
@@ -48,12 +46,12 @@ try{
                 
         #Add the authorization header to the request
         $authorization = @{
-            Authorization = "Bearer $accesstoken";
+            Authorization = "Bearer $($accesstoken)";
             'Content-Type' = "application/json";
             Accept = "application/json";
         }
         $body = $account | ConvertTo-Json -Depth 10
-        $response = Invoke-RestMethod -Uri "https://www.googleapis.com/admin/directory/v1/users/$aRef" -Method PUT -Headers $authorization -Body $body -Verbose:$false
+        $response = Invoke-RestMethod -Uri "https://www.googleapis.com/admin/directory/v1/users/$($aRef)" -Method PUT -Headers $authorization -Body $body -Verbose:$false
     }
     $success = $True;
     $auditMessage = " successfully";
