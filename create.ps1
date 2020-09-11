52416d97bbf43e33e2b3521b1a9407f3d19f1c7e
Mike Sheldon <m.sheldon@tools4ever.com>
2020-09-11 06:17:59 -0700

    Missing Authorization on create

--- create.ps1
+++ create.ps1
@@ -57,7 +57,27 @@ $account = [PSCustomObject]@{
                     })
     suspended = $True
 }
- 
+
+# exchange the refresh token for an access token
+    $requestUri = "https://www.googleapis.com/oauth2/v4/token"
+        
+    $refreshTokenParams = @{
+            client_id=$clientId;
+            client_secret=$clientSecret;
+            redirect_uri=$redirectUri;
+            refresh_token=$refreshToken;
+            grant_type="refresh_token"; # Fixed value
+    };
+    $response = Invoke-RestMethod -Method Post -Uri $requestUri -Body $refreshTokenParams -Verbose:$false
+    $accessToken = $response.access_token
+            
+    #Add the authorization header to the request
+    $authorization = @{
+        Authorization = "Bearer $accesstoken";
+        'Content-Type' = "application/json";
+        Accept = "application/json";
+    }
+
 #Check if account exists (externalId), else create
     $correlationResponse = Invoke-RestMethod -Uri "https://www.googleapis.com/admin/directory/v1/users?customer=my_customer&query=externalId=$($p.ExternalId)&projection=FULL" -Method GET -Headers $authorization -Verbose:$false;
     
