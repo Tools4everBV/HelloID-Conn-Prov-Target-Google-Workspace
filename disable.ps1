043961f1c8abb656c12a1da69d0abf6469a1013e
T4E-JAnderson <75453090+T4E-JAnderson@users.noreply.github.com>
2021-02-02 13:19:40 -0800

    Update disable.ps1

--- disable.ps1
+++ disable.ps1
@@ -1,54 +1,111 @@
-#2020-10-28
-$config = ConvertFrom-Json $configuration;
+#2021-01-25 - Disable Google Account
+$config = ConvertFrom-Json $configuration
  
 #Initialize default properties
-$success = $False;
 $p = $person | ConvertFrom-Json
-$aRef = $accountReference | ConvertFrom-Json;
-$auditMessage = " not disabled successfully";
- 
-#Change mapping here
-$account = [PSCustomObject]@{
-    suspended = $True
-}
- 
-try{
-    if(-Not($dryRun -eq $True)){
-        ### exchange the refresh token for an access token
-        $requestUri = "https://www.googleapis.com/oauth2/v4/token"
-         
-        $refreshTokenParams = @{
-             client_id=$config.clientId;
-             client_secret=$config.clientSecret;
-             redirect_uri=$config.redirectUri;
-             refresh_token=$config.refreshToken;
-             grant_type="refresh_token"; # Fixed value
-        };
-        $response = Invoke-RestMethod -Method Post -Uri $requestUri -Body $refreshTokenParams -Verbose:$false
-        $accessToken = $response.access_token
-                
-        #Add the authorization header to the request
-        $authorization = @{
-            Authorization = "Bearer $accesstoken";
-            'Content-Type' = "application/json";
-            Accept = "application/json";
-        }
-        $body = $account | ConvertTo-Json -Depth 10
-        $response = Invoke-RestMethod -Uri "https://www.googleapis.com/admin/directory/v1/users/$($aRef)" -Method PUT -Headers $authorization -Body $body -Verbose:$false
+$aRef = $accountReference | ConvertFrom-Json
+$success = $False
+$auditMessage = ""
+
+$default_ou = "/Student Accounts/Inactive"
+
+#Target OrgUnitPath
+$calc_ou = ("{0}/{1}/{2}" -f
+    $default_ou,
+    $p.PrimaryContract.Department.ExternalID,
+    $p.custom.GradYear
+    )
+Write-Information ("Target OU: {0}" -f $calc_ou )
+
+#Support Functions
+function google-refresh-accessToken()
+{
+    ### exchange the refresh token for an access token
+    $requestUri = "https://www.googleapis.com/oauth2/v4/token"
+        
+    $refreshTokenParams = @{
+            client_id=$config.clientId
+            client_secret=$config.clientSecret
+            redirect_uri=$config.redirectUri
+            refresh_token=$config.refreshToken
+            grant_type="refresh_token" # Fixed value
+    };
+    $response = Invoke-RestMethod -Method Post -Uri $requestUri -Body $refreshTokenParams -Verbose:$false
+    $accessToken = $response.access_token
+            
+    #Add the authorization header to the request
+    $authorization = [ordered]@{
+        Authorization = "Bearer $accesstoken"
+        'Content-Type' = "application/json"
+        Accept = "application/json"
     }
-    $success = $True;
-    $auditMessage = " successfully";
+    $authorization
+}
+
+#Change mapping here
+$account = @{
+    suspended = $True
+    orgUnitPath = ""
+}
+
+try{
+    $authorization = google-refresh-accessToken
+
+    # Verify Target OU Exists.  Else, use Default OU
+    $splat = @{
+        Uri = ("https://www.googleapis.com/admin/directory/v1/customer/my_customer/orgunits{0}" -f $calc_ou)
+        Method = 'GET'
+        Headers = $authorization
+        Verbose = $False
+    }
+    #  API will error if target OU does not exist.
+    try {
+        $response = Invoke-RestMethod @splat
+        $account.orgUnitPath = $calc_ou
+    }
+    catch {
+        Write-Information ("Target OU Not found.  Using Default OU: {0}" -f $default_ou)
+        $account.orgUnitPath = $default_ou
+    }
+
+    # Update Account
+    if(-Not $dryRun){
+        # Get Previous Account
+        $splat = @{
+            Uri = "https://www.googleapis.com/admin/directory/v1/users/$($aRef)" 
+            Method = 'GET'
+            Headers = $authorization 
+            Verbose = $False
+        }
+        $previousAccount = Invoke-RestMethod @splat
+
+        $splat = @{
+            Uri = "https://www.googleapis.com/admin/directory/v1/users/$($aRef)"
+            Method = 'PUT'
+            Headers = $authorization
+            Body = ($account | ConvertTo-Json -Depth 10)
+            Verbose = $False
+        }
+        $disabledAccount = Invoke-RestMethod @splat
+    }
+    $success = $True
+    $auditMessage = "Disabled account with PrimaryEmail $($newAccount.primaryEmail) and moved to OrgUnit [$($newAccount.orgUnitPath)]"
 }catch{
-    $auditMessage = " : General error $($_)";
-    Write-Error -Verbose $_;  
+    $auditMessage = "Error disabling account with PrimaryEmail $($newAccount.primaryEmail) - Error: $($_)"
+    Write-Error -Verbose $_
 }
  
 #build up result
 $result = [PSCustomObject]@{
-    Success= $success;
-    AccountReference= $aRef;
-    AuditDetails=$auditMessage;
-    Account= $account;
-};
-  
-Write-Output $result | ConvertTo-Json -Depth 10;
+    Success = $success
+    AccountReference = $aRef
+    AuditDetails = $auditMessage
+    Account = $disabledAccount
+    PreviousAccount = $previousAccount
+    
+    ExportData = [PSCustomObject]@{
+        OrgUnitPath = $disabledAccount.orgUnitPath
+    }
+}
+
+Write-Output ($result | ConvertTo-Json -Depth 10)
